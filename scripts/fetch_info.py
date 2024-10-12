520448c0ef1a8326e0aa85e6529353ecc2012ee6
alsmk <alhel444@gmail.com>
2024-10-12 13:01:19 +0600

    formatted

--- scripts/fetch_info.py
+++ scripts/fetch_info.py
@@ -4,27 +4,27 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-token = os.getenv('TOKEN')
-repo = 'alsmk/CI-CD_tasks'
+token = os.getenv("TOKEN")
+repo = "alsmk/CI-CD_tasks"
 base_url = "https://api.github.com"
 
+
 def fetch_info(url):
-    headers = {
-        "Authorization": f"token {token}"
-    }
-    print (token)
+    headers = {"Authorization": f"token {token}"}
 
     response = requests.get(url, headers=headers)
     commits = response.json()
     return commits
 
-def print_commit_info():
-    url=f'{base_url}/repos/{repo}/commits'
-    print(url)
-    dict = fetch_info(url)
-    print(type(dict))
-    print(dict)
 
+def print_commit_info():
+    url = f"{base_url}/repos/{repo}/commits"
+    lis = fetch_info(url)
+    commit = lis[0]
+    print("The latest commit ::")
+    print(f"Commit Message: {commit['commit']['message']}")
+    print(f"Author: {commit['commit']['author']['name']}")
+    print(f"Date: {commit['commit']['author']['date']}")
 
 
 if __name__ == "__main__":
