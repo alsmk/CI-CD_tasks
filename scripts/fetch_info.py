9a4f955a41e3293de85d47a66fb67670496e3777
alsmk <alhel444@gmail.com>
2024-10-12 12:39:33 +0600

    one function has been compleeted

--- scripts/fetch_info.py
+++ scripts/fetch_info.py
@@ -1,2 +1,32 @@
 import requests
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+token = os.getenv('TOKEN')
+repo = 'alsmk/CI-CD_tasks'
+base_url = "https://api.github.com"
+
+def fetch_info(url):
+    headers = {
+        "Authorization": f"token {token}"
+    }
+    print (token)
+
+    response = requests.get(url, headers=headers)
+    commits = response.json()
+    return commits
+
+def print_commit_info():
+    url=f'{base_url}/repos/{repo}/commits'
+    print(url)
+    dict = fetch_info(url)
+    print(type(dict))
+    print(dict)
+
+
+
+if __name__ == "__main__":
+    print_commit_info()
+    # print_pr_info()
