aca6711bb4720090d631cee88b472958fe5c968a
alsmk <alhel444@gmail.com>
2024-10-14 13:54:31 +0600

    new 2 workflows has been added

--- scripts/test.py
+++ scripts/test.py
@@ -4,24 +4,21 @@ import unittest
 
 
 class TestCommitInfo(unittest.TestCase):
-    
+
     def test_committer_name(self):
         repo = "alsmk/CI-CD_tasks"
         base_url = "https://api.github.com"
         url = f"{base_url}/repos/{repo}/commits"
         commits = fetch_info(url)
-        self.assertEqual(commits[0]['commit']['author']['name'], "alsmk")
-
-        
+        self.assertEqual(commits[0]["commit"]["author"]["name"], "alsmk")
 
     def test_fetch_commits(self):
         repo = "alsmk/CI-CD_tasks"
         base_url = "https://api.github.com"
         url = f"{base_url}/repos/{repo}/commits"
         commits = fetch_info(url)
-        self.assertGreater(len(commits), 0)  
+        self.assertGreater(len(commits), 0)
 
 
 if __name__ == "__main__":
     unittest.main()
-
